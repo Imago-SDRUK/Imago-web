489a6285afe28b17a924e685a8c23e88785650d2
artgp <git@arturoguzmanperez.com>
2026-04-20 16:55:37 +0100

    feat: migrate groups to controller

--- apps/data_catalogue/src/routes/(app)/admin/settings/groups/+page.server.ts
+++ apps/data_catalogue/src/routes/(app)/admin/settings/groups/+page.server.ts
@@ -1,77 +1,154 @@
-import { fail, redirect } from '@sveltejs/kit'
-import { authorise, ketoRead } from '$lib/utils/auth/index.js'
-import { get } from '$lib/utils/ckan/ckan.js'
+import { fail } from '@sveltejs/kit'
 import { error } from '@sveltejs/kit'
-import { parseForm } from '$lib/utils/forms/index.js'
-// import { AUTH_GROUPS } from '$lib/globals/auth.js'
+import { formGetStringOrUndefined, parseForm } from '$lib/utils/forms/index.js'
+import { groupCreateController } from '$lib/server/interface/adapters/controllers/groups/create.js'
+import { groupDeleteController } from '$lib/server/interface/adapters/controllers/groups/delete.js'
+import {
+	groupAddUserController,
+	groupRemoveUserController,
+	groupUpdateController
+} from '$lib/server/interface/adapters/controllers/groups/update.js'
+import {
+	groupGetController,
+	groupGetUsersController,
+	groupsGetController
+} from '$lib/server/interface/adapters/controllers/groups/get.js'
+import { usersSearchController } from '$lib/server/interface/adapters/controllers/users/get.js'
+// import { permissionsGetController } from '$lib/server/interface/adapters/controllers/permissions/get.js'
+// import type { Relationships } from '$lib/server/entities/models/permissions.js'
+import type { Group } from '$lib/server/entities/models/groups.js'
 
 export const load = async ({ locals, url }) => {
-	await authorise({
-		namespace: 'Group',
-		relation: 'users',
-		session: locals.session,
-		object: 'admin'
-	})
-	const auth_groups = await ketoRead.getRelationships({
-		namespace: 'Group',
-		pageToken: url.searchParams.get('page_token') ?? undefined,
-		pageSize: 25
-	})
-	// const missing_groups = AUTH_GROUPS.filter((group) =>
-	// 	relationships.relation_tuples?.findIndex((tuple) => tuple.subject_id === group)
-	// )
-	const ckan_groups = await locals.ckan.request(get('group_list', { all_fields: true }))
-	if (!ckan_groups.success) {
-		error(400, { message: 'Error getting CKAN groups', id: '' })
+	const [errors, groups] = await groupsGetController({ session: locals.session })
+	if (errors !== null) {
+		error(500, { message: errors.reason, id: errors.reason })
+	}
+	const edit = url.searchParams.get('edit')
+	let group_users: { first_name: string; last_name: string; email: string; id: string }[] = []
+	// let existing: Relationships | null = null
+	let group: Group | null = null
+	if (edit) {
+		group = await groupGetController({ session: locals.session, id: edit }).then(
+			([errors, users]) => {
+				if (errors !== null) {
+					error(500, { message: errors.reason, id: errors.reason })
+				}
+				return users
+			}
+		)
+		// existing = await permissionsGetController({
+		// 	data: { namespace: 'Group', object: 'admin' },
+		// 	session: locals.session
+		// }).then(([errors, users]) => {
+		// 	if (errors !== null) {
+		// 		error(500, { message: errors.reason, id: errors.reason })
+		// 	}
+		// 	return users
+		// })
+
+		group_users = await groupGetUsersController({ session: locals.session, group_id: edit }).then(
+			([errors, users]) => {
+				if (errors !== null) {
+					error(500, { message: errors.reason, id: errors.reason })
+				}
+				return users
+			}
+		)
 	}
 	return {
-		auth_groups,
-		ckan_groups
+		groups,
+		group_users,
+		group
 	}
 }
 
 export const actions = {
-	create: async ({ locals, request, fetch }) => {
-		if (!locals.session) {
-			return redirect(307, '/')
-		}
+	create: async ({ locals, request }) => {
 		const form = parseForm(await request.formData())
-		const res = await fetch(`/api/v1/groups`, {
-			method: 'POST',
-			headers: {
-				'content-type': 'application/json'
-			},
-			body: JSON.stringify(form)
-		}).catch(() => error(500, { message: 'Error saving group', id: 'err' }))
-		const data = await res.json()
-		if (!res.ok) {
-			return fail(res.status, data)
+		const [errors] = await groupCreateController({ data: form, session: locals.session })
+		if (errors !== null) {
+			console.log(errors)
+			return fail(500, { message: errors.reason })
 		}
 		return {
 			message: 'Group created'
 		}
 	},
-	delete: async ({ locals, request, fetch }) => {
-		if (!locals.session) {
-			return redirect(307, '/')
+	edit: async ({ locals, request }) => {
+		const form = await request.formData()
+		const id = formGetStringOrUndefined({ form, field: 'id' })
+		const data = parseForm(form)
+		const [errors, group] = await groupUpdateController({ id, data: data, session: locals.session })
+		if (errors !== null) {
+			console.log(errors)
+			return fail(500, { message: errors.reason })
 		}
-		const form = parseForm(await request.formData())
-		if ('id' in form === false) {
-			return fail(400, { message: 'You need to provide an ID' })
+		console.log(group)
+		return {
+			message: 'Group updated'
 		}
-		const res = await fetch(`/api/v1/groups/${form.id}`, {
-			method: 'DELETE',
-			headers: {
-				'content-type': 'application/json'
-			},
-			body: JSON.stringify(form)
-		}).catch(() => error(500, { message: 'Error deleting group', id: 'err' }))
-		const data = await res.json()
-		if (!res.ok) {
-			return fail(res.status, data)
+	},
+	delete: async ({ locals, request }) => {
+		const form = await request.formData()
+		const id = formGetStringOrUndefined({ form, field: 'id' })
+		const [errors] = await groupDeleteController({ id, session: locals.session })
+		if (errors !== null) {
+			return fail(500, { message: errors.reason })
 		}
 		return {
 			message: 'Group deleted'
+		}
+	},
+	add_user: async ({ locals, request }) => {
+		const form = await request.formData()
+		const payload = {
+			user_id: formGetStringOrUndefined({ form, field: 'user_id' }),
+			group_id: formGetStringOrUndefined({ form, field: 'group_id' }),
+			relation: formGetStringOrUndefined({ form, field: 'relation' })
+		}
+		const [errors] = await groupAddUserController({
+			relation: payload.relation,
+			session: locals.session,
+			data: payload
+		})
+		if (errors !== null) {
+			console.log(errors)
+			return fail(500, { message: errors.reason })
+		}
+		return {
+			message: `User successfully added`
+		}
+	},
+	remove_user: async ({ locals, request }) => {
+		const form = await request.formData()
+		const payload = {
+			user_id: formGetStringOrUndefined({ form, field: 'user_id' }),
+			group_id: formGetStringOrUndefined({ form, field: 'group_id' })
+		}
+		const [errors] = await groupRemoveUserController({
+			session: locals.session,
+			...payload
+		})
+		if (errors !== null) {
+			return fail(500, { message: errors.reason })
+		}
+		return {
+			message: `User successfully added`
+		}
+	},
+	search_users: async ({ locals, request }) => {
+		const form = await request.formData()
+		const identifier = formGetStringOrUndefined({ form, field: 'identifier' })
+		const [errors, users] = await usersSearchController({
+			session: locals.session,
+			identifier: String(identifier)
+		})
+		if (errors !== null) {
+			return fail(500, { message: errors.reason })
+		}
+		return {
+			message: `Ok`,
+			users
 		}
 	}
 }
--- apps/data_catalogue/src/routes/(app)/admin/settings/groups/+page.svelte
+++ apps/data_catalogue/src/routes/(app)/admin/settings/groups/+page.svelte
@@ -2,77 +2,42 @@
 	import { type IColumnConfig } from '@svar-ui/svelte-grid'
 	import { applyAction, enhance } from '$app/forms'
 	import { debug } from '$lib/globals/dev.svelte.js'
-	import { capitalise, jstr } from '@arturoguzman/art-ui'
+	import { jstr } from '@arturoguzman/art-ui'
 	import {
 		ActionBar,
 		Button,
-		handleSearchParams,
 		Icon,
 		Input,
+		Notice,
 		Paragraph,
 		SectionEdit,
+		Select,
 		Subtitle,
 		Text,
 		Textarea,
 		Title
 	} from '@imago/ui'
-	import type { Relationship } from '@ory/client-fetch'
 	import { onMount } from 'svelte'
 	import Dialog from '$lib/ui/cards/dialog.svelte'
 	import { toggleDialog } from '$lib/utils/ui/index.js'
 	import { notify } from '$lib/stores/notify.js'
 	import BaseTable from '$lib/ui/tables/base_table.svelte'
-	import type { CkanGroup } from '$lib/types/ckan/index.js'
 	import CellText from '$lib/ui/tables/cell_text.svelte'
 	import { page } from '$app/state'
 	import CellEditor from '$lib/ui/tables/cell_editor.svelte'
 	import CellDate from '$lib/ui/tables/cell_date.svelte'
-	import CellGroupToUser from '$lib/ui/tables/cell_group_to_user.svelte'
 	import { DateTime } from 'luxon'
+	import type { Group } from '$lib/server/entities/models/groups.js'
+	import { handleEnhance } from '$lib/utils/forms'
 
 	let { data } = $props()
 	let delete_group = $state('')
-	const auth_groups = $derived(
-		data.auth_groups.relation_tuples?.reduce(
-			(acc, relation) => {
-				if (relation.object in acc) {
-					acc[relation.object].push(relation)
-					return acc
-				}
 
-				acc[relation.object] = [relation]
-				return acc
-			},
-			{} as { [k: string]: Relationship[] }
-		)
-	)
 	onMount(() => {
 		debug.data = data
 	})
-	let auth_groups_columns: (IColumnConfig & { id: keyof Relationship })[] = [
-		// {
-		// 	id: 'namespace',
-		// 	header: 'Namespace',
-		// 	cell: CellText
-		// },
-		// {
-		// 	id: 'object',
-		// 	header: 'Object',
-		// 	cell: CellEditorGroup
-		// },
-		{
-			id: 'relation',
-			header: 'Relation',
-			cell: CellText
-		},
-		{
-			id: 'subject_id',
-			header: 'Subject ID',
-			cell: CellGroupToUser,
-			width: 400
-		}
-	]
-	let ckan_groups_columns: (IColumnConfig & { id: keyof CkanGroup })[] = [
+
+	let columns: (IColumnConfig & { id: keyof Group })[] = [
 		{
 			id: 'title',
 			header: 'Title',
@@ -84,35 +49,39 @@
 			cell: CellText
 		},
 		{
-			id: 'created',
+			id: 'created_at',
 			header: 'Created at',
-			cell: CellDate,
+			cell: CellText,
+			width: 400
+		},
+		{
+			id: 'updated_at',
+			header: 'Updated at',
+			cell: CellText,
 			width: 400
 		}
 	]
 
-	let ckan_groups_selected = $derived(
-		data.ckan_groups.result?.findIndex((group) => group.id === page.url.searchParams.get('edit')) ??
-			-1
+	let selected = $derived(
+		data.groups.findIndex((group) => group.id === page.url.searchParams.get('edit')) ?? -1
 	)
 
-	let ory_groups_selected = $derived(
-		auth_groups &&
-			page.url.searchParams.get('group') &&
-			String(page.url.searchParams.get('group')) in auth_groups
-			? auth_groups[String(page.url.searchParams.get('group'))]
-			: undefined
-	)
 	let edit = $state(false)
+	let selected_user = $state('')
+	let selected_user_delete = $state('')
+	let search_results = $state([])
+	let available_users = $derived(
+		search_results.filter((au) => !data.group_users.find((gu) => gu.id === au.id))
+	)
 </script>
 
 <Title>Groups</Title>
 <div class="tables">
-	<SectionEdit open={ckan_groups_selected !== -1 ? true : undefined}>
+	<SectionEdit open={selected !== -1 ? true : undefined}>
 		{#snippet leftCol()}
 			<ActionBar>
 				{#snippet left()}
-					<Subtitle>CKAN</Subtitle>
+					<Subtitle>Groups</Subtitle>
 				{/snippet}
 				{#snippet right()}
 					<Button
@@ -126,12 +95,13 @@
 				{/snippet}
 			</ActionBar>
 			<div class="groups">
-				<BaseTable data={data.ckan_groups.result} columns={ckan_groups_columns}></BaseTable>
+				<BaseTable data={data.groups} {columns}></BaseTable>
 			</div>
 		{/snippet}
 		{#snippet rightCol()}
 			<div class="edit">
-				{#if data.ckan_groups.result[ckan_groups_selected]}
+				{#if data.group}
+					{@const group = data.group}
 					<ActionBar>
 						{#snippet left()}
 							<Button width="auto" href={page.url.pathname}>
@@ -150,95 +120,239 @@
 							</Button>
 						{/snippet}
 					</ActionBar>
-					{@const group = data.ckan_groups.result[ckan_groups_selected]}
-					<ActionBar>
-						{#snippet left()}
-							<Subtitle>{group.title}</Subtitle>
-						{/snippet}
-						{#snippet right()}
-							<Button
-								active={edit}
-								onclick={() => {
-									edit = !edit
-								}}
-							>
-								<Icon icon={{ icon: 'edit', set: 'tabler' }} />
-							</Button>
-						{/snippet}
-					</ActionBar>
-					{#if !edit}
-						<div class="card">
-							<Paragraph>Title: {group.title}</Paragraph>
-							<Paragraph
-								>Created at: {DateTime.fromISO(group.created)
-									.setLocale('en-gb')
-									.toLocaleString(DateTime.DATETIME_MED)}</Paragraph
-							>
-							<Paragraph>Description: {group.description}</Paragraph>
-						</div>
-					{/if}
-					{#if edit}
-						<div class="card">
-							<form action="?/edit" method="post" use:enhance>
-								<Input label="Title">
-									<Text value={group.title}></Text>
-								</Input>
-								{#key group}
-									<Input label="Description">
-										<Textarea name="description" bind:value={group.description}></Textarea>
+					<div class="section">
+						<ActionBar>
+							{#snippet left()}
+								<Subtitle>{group.title}</Subtitle>
+							{/snippet}
+							{#snippet right()}
+								<Button
+									active={edit}
+									onclick={() => {
+										edit = !edit
+									}}
+								>
+									<Icon icon={{ icon: 'edit', set: 'tabler' }} />
+								</Button>
+							{/snippet}
+						</ActionBar>
+						{#if !edit}
+							<div class="card">
+								<Paragraph>Title: {group.title}</Paragraph>
+								<Paragraph
+									>Created at: {DateTime.fromJSDate(group.created_at)
+										.setLocale('en-gb')
+										.toLocaleString(DateTime.DATETIME_MED)}</Paragraph
+								>
+								<Paragraph>Visibility: {group.visibility}</Paragraph>
+								<Paragraph>Description: {group.description}</Paragraph>
+							</div>
+						{/if}
+
+						{#if edit}
+							<div class="card">
+								<form
+									action="?/edit"
+									method="post"
+									use:enhance={() => {
+										return async ({ result, update }) => {
+											if (result.type === 'error') {
+												console.log(result)
+												notify.send({ message: result.error.message })
+											}
+											if ('data' in result && result.data) {
+												if ('errors' in result.data) {
+													notify.send(String(jstr(result.data.errors)))
+												}
+												if ('message' in result.data) {
+													notify.send(String(result.data.message))
+												}
+											}
+											if (result.type === 'redirect') {
+												applyAction(result)
+											}
+											update({ reset: true, invalidateAll: true })
+										}
+									}}
+								>
+									<input type="hidden" name="id" value={group.id} />
+									<Input label="Title">
+										<Text value={group.title} name="title"></Text>
 									</Input>
-								{/key}
-								<div class="buttons">
-									<Button
-										type="button"
-										onclick={() => {
-											edit = false
-										}}>Cancel</Button
-									>
-									<Button>Save</Button>
-								</div>
-							</form>
+
+									<Input label="Visibility">
+										<Select
+											name="visibility"
+											value={group.visibility}
+											options={[
+												{ label: 'Public', value: 'public' },
+												{ label: 'Private', value: 'private' }
+											]}
+										></Select>
+									</Input>
+									{#key group}
+										<Input label="Description">
+											<Textarea name="description" bind:value={group.description}></Textarea>
+										</Input>
+									{/key}
+									<div class="buttons">
+										<Button
+											type="button"
+											onclick={() => {
+												edit = false
+											}}>Cancel</Button
+										>
+										<Button>Save</Button>
+									</div>
+								</form>
+							</div>
+						{/if}
+					</div>
+					<div class="section">
+						<ActionBar>
+							{#snippet left()}
+								<Subtitle>Members</Subtitle>
+							{/snippet}
+							{#snippet right()}
+								<Paragraph>Current: {data.group_users.length}</Paragraph>
+							{/snippet}
+						</ActionBar>
+						{#if data.group_users.length === 0}
+							<Notice level="info">
+								<Paragraph size="xs">This group doesn't have any members.</Paragraph>
+							</Notice>
+						{/if}
+						<div class="buttons-multiple">
+							{#each data.group_users as user}
+								{#if !edit}
+									<Paragraph style="label" size="xs">
+										{user?.email}
+									</Paragraph>
+								{/if}
+							{/each}
 						</div>
-					{/if}
+						{#if edit}
+							<div class="card">
+								<Subtitle>Add users</Subtitle>
+								<form
+									action="?/search_users"
+									method="post"
+									use:enhance={() => {
+										return async ({ result }) => {
+											if (result.type === 'success') {
+												if (result.data?.users && Array.isArray(result.data.users)) {
+													const filtered = result.data.users.filter(
+														(au) => !data.group_users.find((gu) => gu.id === au.id)
+													)
+													// available_users = [...filtered]
+													search_results = result.data.users
+												}
+											}
+										}
+									}}
+								>
+									<div class="search-bar">
+										<Input>
+											<Text name="identifier"></Text>
+										</Input>
+										<Button>Search</Button>
+									</div>
+								</form>
+
+								{#if selected_user !== ''}
+									{@const user = available_users.find((user) => user.id === selected_user)}
+									<div class="relation-card">
+										<form
+											action="?/add_user"
+											method="post"
+											use:enhance={handleEnhance({
+												onsuccess: () => {
+													selected_user = ''
+												}
+											})}
+										>
+											<input type="hidden" name="user_id" value={user.id} />
+											<input type="hidden" name="group_id" value={group.id} />
+											<Paragraph>{user.email}</Paragraph>
+											<Select
+												label="Relation"
+												required
+												name="relation"
+												options={[
+													{ label: 'User', value: 'users' },
+													{ label: 'Admin', value: 'admins' }
+												]}
+											></Select>
+											<div class="buttons">
+												<Button
+													type="button"
+													onclick={() => {
+														selected_user = ''
+													}}>Cancel</Button
+												>
+												<Button>Add</Button>
+											</div>
+										</form>
+									</div>
+								{/if}
+								<div class="buttons-multiple">
+									{#each available_users as user}
+										<Button
+											onclick={() => {
+												selected_user = user.id
+											}}>{user.email}</Button
+										>
+									{/each}
+								</div>
+							</div>
+
+							{#if selected_user_delete !== ''}
+								{@const user = data.group_users.find((user) => user.id === selected_user_delete)}
+								{#if user}
+									<div class="relation-card">
+										<form
+											action="?/remove_user"
+											method="post"
+											use:enhance={handleEnhance({
+												onsuccess: () => {
+													selected_user_delete = ''
+												}
+											})}
+										>
+											<input type="hidden" name="user_id" value={user.id} />
+											<input type="hidden" name="group_id" value={group.id} />
+											<Paragraph>{user.email}</Paragraph>
+											<div class="buttons">
+												<Button
+													type="button"
+													onclick={() => {
+														selected_user_delete = ''
+													}}>Cancel</Button
+												>
+												<Button>Remove</Button>
+											</div>
+										</form>
+									</div>
+								{/if}
+							{/if}
+							<div class="buttons-multiple">
+								{#each data.group_users as user}
+									{#if selected_user_delete !== user.id}
+										<Button
+											onclick={() => {
+												selected_user_delete = user.id
+											}}
+										>
+											<Icon icon={{ icon: 'edit', set: 'tabler' }}></Icon>
+											{user.email}</Button
+										>
+									{/if}
+								{/each}
+							</div>
+						{/if}
+					</div>
 				{/if}
 			</div>
-		{/snippet}
-	</SectionEdit>
-
-	<SectionEdit open={ory_groups_selected ? true : undefined}>
-		{#snippet leftCol()}
-			<Subtitle>Auth groups</Subtitle>
-			{#if auth_groups}
-				<div class="groups">
-					{#each Object.entries(auth_groups) as [group, relations]}
-						<!-- <Button -->
-						<!-- 	active={page.url.searchParams.get('group') === group} -->
-						<!-- 	href={handleSearchParams({ -->
-						<!-- 		toggle: [{ key: 'group', value: group }], -->
-						<!-- 		url: page.url -->
-						<!-- 	})} -->
-						<!-- > -->
-						<Subtitle>{capitalise(group)}</Subtitle>
-						<!-- </Button> -->
-						<BaseTable data={relations} columns={auth_groups_columns}></BaseTable>
-					{/each}
-					{#if data.auth_groups.next_page_token}
-						<Button
-							href={handleSearchParams({
-								add: [{ key: 'page_token', value: data.auth_groups.next_page_token, set: true }],
-								url: page.url
-							})}>Next</Button
-						>
-					{/if}
-				</div>
-			{/if}
-		{/snippet}
-		{#snippet rightCol()}
-			{#if ory_groups_selected}
-				<div class="edit">
-					<pre>{jstr(ory_groups_selected)}</pre>
-				</div>
-			{/if}
 		{/snippet}
 	</SectionEdit>
 </div>
@@ -313,7 +427,7 @@
 		<Subtitle>Add group</Subtitle>
 		<div class="inputs">
 			<Input label="Title">
-				<Text name="display_name"></Text>
+				<Text name="title"></Text>
 			</Input>
 			<Input label="Description">
 				<Textarea name="description"></Textarea>
@@ -359,9 +473,37 @@
 		justify-content: space-between;
 		gap: 1rem;
 	}
+
+	.buttons-multiple {
+		display: flex;
+		gap: 0.5rem;
+		flex-wrap: wrap;
+	}
 	form {
 		display: flex;
 		flex-direction: column;
 		gap: 0.5rem;
+	}
+	.search-bar {
+		display: flex;
+		gap: 1rem;
+	}
+	.edit {
+		display: flex;
+		flex-direction: column;
+		gap: 2rem;
+	}
+	.section {
+		display: flex;
+		flex-direction: column;
+		gap: 0.5rem;
+	}
+	.relation-card {
+		display: flex;
+		flex-direction: column;
+		gap: 0.5rem;
+		background-color: var(--background-muted);
+		padding: 1rem;
+		border: 1px solid var(--border);
 	}
 </style>
