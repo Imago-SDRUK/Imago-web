de54323b2bba3e9f00f2c77a79c8ff6a9bdc8e6c
artgp <git@arturoguzmanperez.com>
2026-04-20 16:55:55 +0100

    fix: delete permissions routes
    fix: delete permissions routes
    
    fix: delete permissions routes

--- apps/data_catalogue/src/routes/(app)/admin/settings/permissions/+page.server.ts
+++ apps/data_catalogue/src/routes/(app)/admin/settings/permissions/+page.server.ts
@@ -1,18 +1,1 @@
-import { ketoRead } from '$lib/utils/auth'
-
-export const load = async () => {
-	const users = await ketoRead.getRelationships({ namespace: 'User' })
-	const groups = await ketoRead.getRelationships({ namespace: 'Group' })
-	const datasets = await ketoRead.getRelationships({ namespace: 'Dataset' })
-	const resources = await ketoRead.getRelationships({ namespace: 'Resource' })
-	const resource_versions = await ketoRead.getRelationships({ namespace: 'ResourceVersion' })
-	const endpoints = await ketoRead.getRelationships({ namespace: 'Endpoint' })
-	return {
-		users,
-		groups,
-		datasets,
-		resources,
-		resource_versions,
-		endpoints
-	}
-}
+export const load = async () => {}
