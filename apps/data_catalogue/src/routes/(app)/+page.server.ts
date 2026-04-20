c7979d5f1badd3f88887da52e972dd57372d6449
artgp <git@arturoguzmanperez.com>
2026-04-20 17:00:25 +0100

    feat: migrate landing to controllers

--- apps/data_catalogue/src/routes/(app)/+page.server.ts
+++ apps/data_catalogue/src/routes/(app)/+page.server.ts
@@ -1,8 +1,19 @@
-import { get } from '$lib/utils/ckan/ckan.js'
-import type { PageServerLoadEvent } from './$types.js'
-export const load = async ({ locals }: PageServerLoadEvent) => {
+import { datasetsGetCountController } from '$lib/server/interface/adapters/controllers/datasets/get.js'
+import { error } from '@sveltejs/kit'
+import { tagsGetCountController } from '$lib/server/interface/adapters/controllers/tags/get.js'
+export const load = async () => {
 	return {
-		package_count: (await locals.ckan.request(get('package_list'))).result.length,
-		tag_count: (await locals.ckan.request(get('tag_list'))).result.length
+		package_count: await datasetsGetCountController().then(([errors, count]) => {
+			if (errors !== null) {
+				error(400, { message: `There's been an issue getting the package count`, id: '' })
+			}
+			return count
+		}),
+		tag_count: await tagsGetCountController().then(([errors, count]) => {
+			if (errors !== null) {
+				error(400, { message: `There's been an issue getting the tags count`, id: '' })
+			}
+			return count
+		})
 	}
 }
