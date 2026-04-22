c06c15a6482fa468d6e6af817c0177b224ab67f2
artgp <git@arturoguzmanperez.com>
2026-04-22 19:26:22 +0100

    fix: handle errs

--- apps/data_catalogue/src/lib/server/infrastructure/repositories/resources/drizzle.ts
+++ apps/data_catalogue/src/lib/server/infrastructure/repositories/resources/drizzle.ts
@@ -1,123 +1,169 @@
 import { db } from '$lib/db'
 import { resource_versions, resources } from '$lib/db/schema'
 import type { ResourceRepository } from '$lib/server/application/repositories/resource'
+import { err, ok } from '$lib/server/entities/errors'
 import { handleDBError } from '$lib/utils/db'
-import { error } from '@sveltejs/kit'
 import { and, desc, eq } from 'drizzle-orm'
 
 const getResource: ResourceRepository['getResource'] = async ({ id }) => {
-	const resource = await db
-		.select()
-		.from(resources)
-		.where(eq(resources.id, id))
-		.limit(1)
-		.catch(handleDBError('Resource not found'))
-	if (resource.length === 1) {
-		return resource[0]
+	try {
+		const resource = await db
+			.select()
+			.from(resources)
+			.where(eq(resources.id, id))
+			.limit(1)
+			.catch(handleDBError('Resource not found'))
+		if (resource.length === 1) {
+			return ok(resource[0])
+		}
+		return err({ reason: 'Not Found' })
+	} catch (_err) {
+		return err({ reason: 'Unexpected', errors: _err })
 	}
-	error(404, { message: 'Resource not found', id: '' })
 }
 const getResourceVersion: ResourceRepository['getResourceVersion'] = async ({ id, version }) => {
-	const resource_version = await db
-		.select()
-		.from(resource_versions)
-		.where(and(eq(resource_versions.resource, id), eq(resource_versions.version, version)))
-		.orderBy(desc(resource_versions.created_by))
-		.limit(1)
-		.catch(handleDBError('No version exists for this resource'))
-	if (resource_version.length === 1) {
-		return resource_version[0]
+	try {
+		const resource_version = await db
+			.select()
+			.from(resource_versions)
+			.where(and(eq(resource_versions.resource, id), eq(resource_versions.version, version)))
+			.orderBy(desc(resource_versions.created_by))
+			.limit(1)
+			.catch(handleDBError('No version exists for this resource'))
+		if (resource_version.length === 1) {
+			return ok(resource_version[0])
+		}
+		return err({ reason: 'Not Found' })
+	} catch (_err) {
+		return err({ reason: 'Unexpected', errors: _err })
 	}
-	error(404, { message: 'Resource version not found', id: '' })
 }
 const getResources: ResourceRepository['getResources'] = async ({ limit, offset }) => {
-	const results = await db
-		.select()
-		.from(resources)
-		.limit(limit)
-		.offset(offset)
-		.catch(handleDBError('Resource not found'))
-	return results
+	try {
+		const results = await db
+			.select()
+			.from(resources)
+			.limit(limit)
+			.offset(offset)
+			.catch(handleDBError('Resource not found'))
+		return ok(results)
+	} catch (_err) {
+		return err({ reason: 'Unexpected', errors: _err })
+	}
 }
 
 const createResource: ResourceRepository['createResource'] = async ({ data }) => {
-	const resource = await db
-		.insert(resources)
-		.values({
-			created_by: data.created_by,
-			updated_by: data.updated_by,
-			title: data.title,
-			id: data.id,
-			description: data.description
-		})
-		.returning()
-		.catch(handleDBError('Error creating the resource'))
-	if (resource.length === 1) {
-		return resource[0]
+	try {
+		const resource = await db
+			.insert(resources)
+			.values({
+				created_by: data.created_by,
+				updated_by: data.updated_by,
+				title: data.title,
+				id: data.id,
+				description: data.description
+			})
+			.returning()
+			.catch(handleDBError('Error creating the resource'))
+		if (resource.length === 1) {
+			return ok(resource[0])
+		}
+		return err({ reason: 'Not Found' })
+	} catch (_err) {
+		return err({ reason: 'Unexpected', errors: _err })
 	}
-	error(404, { message: 'Error creating the resource', id: '' })
 }
 
 const createResourceVersion: ResourceRepository['createResourceVersion'] = async ({ data }) => {
-	const resource_version = await db
-		.insert(resource_versions)
-		.values(data)
-		.returning()
-		.catch(handleDBError('Error creating the resource'))
-	if (resource_version.length === 1) {
-		return resource_version[0]
+	try {
+		const resource_version = await db
+			.insert(resource_versions)
+			.values(data)
+			.returning()
+			.catch(handleDBError('Error creating the resource'))
+		if (resource_version.length === 1) {
+			return ok(resource_version[0])
+		}
+		return err({ reason: 'Not Found' })
+	} catch (_err) {
+		return err({ reason: 'Unexpected', errors: _err })
 	}
-	error(404, { message: 'Error creating the resource', id: '' })
 }
 
 const getResourceVersions: ResourceRepository['getResourceVersions'] = async ({ id }) => {
-	const versions = await db
-		.select()
-		.from(resource_versions)
-		.where(eq(resource_versions.resource, id))
-		.orderBy(desc(resource_versions.created_at))
-		.catch(handleDBError('No version exists for this resource'))
-	return versions
+	try {
+		const versions = await db
+			.select()
+			.from(resource_versions)
+			.where(eq(resource_versions.resource, id))
+			.orderBy(desc(resource_versions.created_at))
+			.catch(handleDBError('No version exists for this resource'))
+		return ok(versions)
+	} catch (_err) {
+		return err({ reason: 'Unexpected', errors: _err })
+	}
 }
 
-const deleteResource = async ({ id }: { id: string }) => {
-	await db
-		.delete(resources)
-		.where(eq(resources.id, id))
-		.catch(handleDBError('Error deleting resource'))
+const deleteResource: ResourceRepository['deleteResource'] = async ({ id }: { id: string }) => {
+	try {
+		await db
+			.delete(resources)
+			.where(eq(resources.id, id))
+			.catch(handleDBError('Error deleting resource'))
+		return ok(null)
+	} catch (_err) {
+		return err({ reason: 'Unexpected', errors: _err })
+	}
 }
 
-const deleteResourceVersion = async ({ id }: { id: string }) => {
-	await db
-		.delete(resource_versions)
-		.where(eq(resource_versions.id, id))
-		.catch(handleDBError('Error deleting resource version'))
+const deleteResourceVersion: ResourceRepository['deleteResourceVersion'] = async ({
+	id
+}: {
+	id: string
+}) => {
+	try {
+		await db
+			.delete(resource_versions)
+			.where(eq(resource_versions.id, id))
+			.catch(handleDBError('Error deleting resource version'))
+		return ok(null)
+	} catch (_err) {
+		return err({ reason: 'Unexpected', errors: _err })
+	}
 }
 
 const updateResource: ResourceRepository['updateResource'] = async ({ id, data }) => {
-	const resource = await db
-		.update(resources)
-		.set(data)
-		.where(eq(resources.id, id))
-		.returning()
-		.catch(handleDBError('Error updating resource'))
-	if (resource.length === 1) {
-		return resource[0]
+	try {
+		const resource = await db
+			.update(resources)
+			.set(data)
+			.where(eq(resources.id, id))
+			.returning()
+			.catch(handleDBError('Error updating resource'))
+		if (resource.length === 1) {
+			return ok(resource[0])
+		}
+		return err({ reason: 'Not Found' })
+	} catch (_err) {
+		return err({ reason: 'Unexpected', errors: _err })
 	}
-	error(404, { message: 'Error updating the resource', id: '' })
 }
 
 const updateVersion: ResourceRepository['updateVersion'] = async ({ id, data }) => {
-	const version = await db
-		.update(resource_versions)
-		.set(data)
-		.where(eq(resource_versions.id, id))
-		.returning()
-		.catch(handleDBError('Error updating resource version'))
-	if (version.length === 1) {
-		return version[0]
+	try {
+		const version = await db
+			.update(resource_versions)
+			.set(data)
+			.where(eq(resource_versions.id, id))
+			.returning()
+			.catch(handleDBError('Error updating resource version'))
+		if (version.length === 1) {
+			return ok(version[0])
+		}
+		return err({ reason: 'Not Found' })
+	} catch (_err) {
+		return err({ reason: 'Unexpected', errors: _err })
 	}
-	error(404, { message: 'Error updating the resource version', id: '' })
 }
 
 // const getResourcesByDataset: ResourceRepository['getResourcesByDataset'] = async ({ id }) => {
--- apps/data_catalogue/src/lib/server/infrastructure/services/storage/azure.ts
+++ apps/data_catalogue/src/lib/server/infrastructure/services/storage/azure.ts
@@ -1,5 +1,6 @@
 import { env } from '$env/dynamic/private'
 import type { StorageService } from '$lib/server/application/services/storage'
+import { err, ok } from '$lib/server/entities/errors'
 import { createSASReadString } from '$lib/utils/files/azure'
 import {
 	BlobSASPermissions,
@@ -24,23 +25,27 @@ export const loadStorageClient = (): { client: BlobServiceClient; container_name
 }
 
 export const getUploadUrl: StorageService['getUploadUrl'] = async ({ filename }) => {
-	const client = loadStorageClient()
-	const duration = 5
-	const container_name = client.container_name
-	const permissions = new BlobSASPermissions()
-	permissions.write = true
+	try {
+		const client = loadStorageClient()
+		const duration = 5
+		const container_name = client.container_name
+		const permissions = new BlobSASPermissions()
+		permissions.write = true
 
-	const now = new Date()
-	const expiry_datetime = new Date(now.setMinutes(now.getMinutes() + duration))
-	const blob_sas = {
-		containerName: container_name,
-		permissions,
-		expiresOn: expiry_datetime
+		const now = new Date()
+		const expiry_datetime = new Date(now.setMinutes(now.getMinutes() + duration))
+		const blob_sas = {
+			containerName: container_name,
+			permissions,
+			expiresOn: expiry_datetime
+		}
+		const container_client = client.client.getContainerClient(container_name)
+		const blob_client = container_client.getBlockBlobClient(filename)
+		const result = await blob_client.generateSasUrl(blob_sas)
+		return ok(result)
+	} catch (_err) {
+		return err({ reason: 'Unexpected', errors: _err })
 	}
-	const container_client = client.client.getContainerClient(container_name)
-	const blob_client = container_client.getBlockBlobClient(filename)
-	const result = await blob_client.generateSasUrl(blob_sas)
-	return result
 }
 
 export const getDownloadUrl: StorageService['getDownloadUrl'] = async ({
@@ -48,19 +53,33 @@ export const getDownloadUrl: StorageService['getDownloadUrl'] = async ({
 }: {
 	filename: string
 }) => {
-	const client = loadStorageClient()
-	const container_name = client.container_name
-	const container_client = client.client.getContainerClient(container_name)
-	const blob_client = container_client.getBlockBlobClient(filename)
-	return blob_client.url + '?' + createSASReadString({ client: client.client, container_name })
+	try {
+		const client = loadStorageClient()
+		const container_name = client.container_name
+		const container_client = client.client.getContainerClient(container_name)
+		const blob_client = container_client.getBlockBlobClient(filename)
+		return ok(
+			blob_client.url + '?' + createSASReadString({ client: client.client, container_name })
+		)
+	} catch (_err) {
+		return err({ reason: 'Unexpected', errors: _err })
+	}
 }
 
-export const deleteFile = async ({ filename }: { filename: string }) => {
-	const client = loadStorageClient()
-	const container_client = client.client.getContainerClient(client.container_name)
-	const blob_client = container_client.getBlockBlobClient(filename)
-	const res = await blob_client.deleteIfExists({ deleteSnapshots: 'include' })
-	return res.succeeded
+export const deleteFile: StorageService['deleteFile'] = async ({
+	filename
+}: {
+	filename: string
+}) => {
+	try {
+		const client = loadStorageClient()
+		const container_client = client.client.getContainerClient(client.container_name)
+		const blob_client = container_client.getBlockBlobClient(filename)
+		const res = await blob_client.deleteIfExists({ deleteSnapshots: 'include' })
+		return ok(res.succeeded)
+	} catch (_err) {
+		return err({ reason: 'Unexpected', errors: _err })
+	}
 }
 
 export const azureStorageServiceInfrastructure: StorageService = {
