6b45c80110dcf9f6acbe8e537dc0355c818572bf
artgp <git@arturoguzmanperez.com>
2026-04-24 14:56:31 +0100

    chore: pass config

--- apps/data_catalogue/src/routes/(app)/datasets/[...id]/+page.server.ts
+++ apps/data_catalogue/src/routes/(app)/datasets/[...id]/+page.server.ts
@@ -5,7 +5,11 @@ import {
 import { error } from '@sveltejs/kit'
 
 export const load = async ({ locals, params }) => {
-	const [errs, dataset] = await datasetGetController({ id: params.id, session: locals.session })
+	const [errs, dataset] = await datasetGetController({
+		id: params.id,
+		session: locals.session,
+		configuration: locals.configuration
+	})
 	if (errs !== null) {
 		error(400, { message: `There's been an error retreiving this dataset`, id: '' })
 	}
@@ -14,14 +18,14 @@ export const load = async ({ locals, params }) => {
 		error(400, { message: `There's been an error retreiving this dataset`, id: '' })
 	}
 	const [errors, activities] = await datasetGetActivityController({
-		id: params.id,
-		session: locals.session
+		id: dataset.id,
+		session: locals.session,
+		configuration: locals.configuration
 	})
 	if (errors !== null) {
 		error(400, { message: errors.reason, id: errors.reason })
 	}
 	return {
-		dataset,
 		activities
 	}
 }
