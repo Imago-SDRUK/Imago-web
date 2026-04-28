50fbca8fa6d458df290bd07ed26b9ab0fe04db63
artgp <git@arturoguzmanperez.com>
2026-04-28 17:28:38 +0100

    fix: downloads cleanup

--- apps/data_catalogue/src/lib/server/application/use_cases/downloads/create.ts
+++ apps/data_catalogue/src/lib/server/application/use_cases/downloads/create.ts
@@ -1,24 +1,31 @@
 import type { AppContext } from '$lib/server/application/context'
 import type { DownloadsRepository } from '$lib/server/application/repositories/downloads'
+import type { ResourceRepository } from '$lib/server/application/repositories/resource'
 import { err, ok } from '$lib/server/entities/errors'
-import type { DownloadRequest } from '$lib/server/entities/models/downloads'
+import { downloads } from '$lib/server/entities/models/downloads'
+import { type } from 'arktype'
+import { createInsertSchema } from 'drizzle-arktype'
 
 // NOTE: maybe make this a sub use case instead?
 export const downloadsCreateUseCase = async ({
-	data,
 	session,
 	downloads_repository,
 	configuration,
-	authorisation_module
+	authorisation_module,
+	version_id,
+	resource_id,
+	resource_repository
 }: {
-	data: DownloadRequest
+	version_id: string
+	resource_id: string
 	downloads_repository: DownloadsRepository
+	resource_repository: ResourceRepository
 } & AppContext) => {
 	const [errors, permission] = await authorisation_module.authorise({
-		actor: session.identity.id,
-		namespace: 'Resource',
-		object: data.resource,
+		namespace: 'ResourceVersion',
+		object: version_id,
 		permits: 'read',
+		actor: session.identity.id,
 		configuration
 	})
 	if (errors) {
@@ -27,9 +34,22 @@ export const downloadsCreateUseCase = async ({
 	if (!permission.allowed) {
 		return err({ reason: 'Unauthorised' })
 	}
-	const [d_errors, download] = await downloads_repository.registerDownload({ data })
+	const schema = createInsertSchema(downloads)
+	const validated = schema({
+		version: version_id,
+		resource: resource_id,
+		user: session.identity.id
+	})
+	if (validated instanceof type.errors) {
+		return err({ reason: 'Unauthorised', message: validated.summary })
+	}
+	const [d_errors, download] = await downloads_repository.registerDownload({ data: validated })
 	if (d_errors !== null) {
 		return err(d_errors)
+	}
+	const [v_errors] = await resource_repository.updateVersionAddDownload({ id: version_id })
+	if (v_errors !== null) {
+		return err(v_errors)
 	}
 	return ok(download)
 }
