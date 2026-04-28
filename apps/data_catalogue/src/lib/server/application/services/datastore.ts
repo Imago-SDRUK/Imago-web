0a488ab1642c660a1aa19b5b5bbb9267885dbd67
artgp <git@arturoguzmanperez.com>
2026-04-28 17:24:35 +0100

    feat: add datastore create validation

--- apps/data_catalogue/src/lib/server/application/services/datastore.ts
+++ apps/data_catalogue/src/lib/server/application/services/datastore.ts
@@ -1,18 +1,22 @@
 import type { ErrTypes } from '$lib/server/entities/errors'
-import type { CSVW } from '$lib/server/entities/models/datastore'
+import type { CkanDatastore, CkanDatastoreRequest } from '$lib/server/entities/models/datastore'
 
 export type DatastoreService = {
-	getStructuralMetadata: ({ id }: { id: string }) => Promise<[ErrTypes, null] | [null, CSVW]>
-	setStructuralMetadata: ({
+	getStructuralMetadata: ({
 		id
 	}: {
 		id: string
-		metadata: CSVW
-	}) => Promise<[ErrTypes, null] | [null, CSVW[]]>
+	}) => Promise<[ErrTypes, null] | [null, CkanDatastore]>
+	deleteStructuralMetadata: ({ id }: { id: string }) => Promise<[ErrTypes, null] | [null, null]>
+	setStructuralMetadata: ({
+		metadata
+	}: {
+		metadata: CkanDatastoreRequest
+	}) => Promise<[ErrTypes, null] | [null, CkanDatastore]>
 	updateStructuralMetadata: ({
 		id
 	}: {
 		id: string
-		metadata: Partial<CSVW>
-	}) => Promise<[ErrTypes, null] | [null, CSVW[]]>
+		metadata: CkanDatastoreRequest
+	}) => Promise<[ErrTypes, null] | [null, CkanDatastore]>
 }
--- apps/data_catalogue/src/lib/server/application/use_cases/datastore/create.ts
+++ apps/data_catalogue/src/lib/server/application/use_cases/datastore/create.ts
@@ -1,9 +1,11 @@
 import type { AppContext } from '$lib/server/application/context'
 import type { DatastoreService } from '$lib/server/application/services/datastore'
-import { err, ok } from '$lib/server/entities/errors'
-import type { CSVW } from '$lib/server/entities/models/datastore'
+import { err, ok, type ErrTypes } from '$lib/server/entities/errors'
+import { CkanDatastore, CSVW } from '$lib/server/entities/models/datastore'
+import { csvwToDatastore } from '$lib/server/entities/utils/datastore'
+import { type } from 'arktype'
 
-export const datastoreCreateUseCase = async ({
+export const datastoreCSVWtoCKANCreateUseCase = async ({
 	resource_id,
 	datastore_service,
 	metadata,
@@ -28,8 +30,58 @@ export const datastoreCreateUseCase = async ({
 	if (!permission.allowed) {
 		return err({ reason: 'Unauthorised' })
 	}
-	return await datastore_service
-		.setStructuralMetadata({ id: resource_id, metadata })
-		.then((res) => ok(res))
-		.catch((_err) => err({ reason: 'Unexpected', error: _err }))
+	const validated = CSVW(metadata)
+	if (validated instanceof type.errors) {
+		return err({ reason: 'Invalid Data', message: validated.summary, id: 'invalid-metadata' })
+	}
+	const converted = csvwToDatastore({ id: resource_id, csvw: validated, force: true })
+	// return ok(converted)
+	const results = await Promise.all(
+		converted.map((table) => datastore_service.setStructuralMetadata({ metadata: table }))
+	)
+
+	const { datastore_errors, datastore } = results.reduce(
+		(acc, [errors, data]) => {
+			if (errors !== null) {
+				acc.datastore_errors.push(errors)
+			}
+			if (data !== null) {
+				acc.datastore.push(data)
+			}
+			return acc
+		},
+		{ datastore_errors: [], datastore: [] } as {
+			datastore_errors: ErrTypes[]
+			datastore: CkanDatastore[]
+		}
+	)
+	// const record = csvwToDatastore({
+	// 	id: id,
+	// 	csvw: metadata,
+	// 	force: true
+	// 	// delete_fields: true
+	// })
+	// const result = await Promise.all(
+	// 	record.map(async (table) => {
+	// 		return ckan.request(create('datastore_create', table))
+	// 	})
+	// )
+	// const { errors, data } = result.reduce(
+	// 	(acc, res) => {
+	// 		if (!res.success) {
+	// 			acc.errors.push(handleCkanError(res))
+	// 			return acc
+	// 		}
+	// 		acc.data.push(datastoreToCsvw(res.result))
+	// 		return acc
+	// 	},
+	// 	{ errors: [], data: [] } as { errors: ErrTypes[]; data: CSVW[] }
+	// )
+	// if (errors.length > 0) {
+	// 	return err(errors[0])
+	// }
+	if (datastore_errors.length > 0) {
+		return err(datastore_errors[0])
+	}
+	return ok(datastore)
 }
