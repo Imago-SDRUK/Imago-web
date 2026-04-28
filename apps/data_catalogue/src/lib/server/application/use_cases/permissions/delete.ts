9208f13bcace523161296d578040ee2439cc702f
artgp <git@arturoguzmanperez.com>
2026-04-28 17:27:33 +0100

    feat: add delete permissions use case

--- apps/data_catalogue/src/lib/server/application/use_cases/permissions/delete.ts
+++ apps/data_catalogue/src/lib/server/application/use_cases/permissions/delete.ts
@@ -37,3 +37,35 @@ export const permissionDeleteUseCase = async ({
 	}
 	return ok(permissions)
 }
+
+export const permissionsDeleteUseCase = async ({
+	data,
+	session,
+	authorisation_module,
+	configuration
+}: {
+	data: Partial<PermissionRequest>
+} & AppContext) => {
+	const [errors, permission] = await authorisation_module.authorise({
+		actor: session.identity.id,
+		namespace: 'Action',
+		object: 'permissions',
+		permits: 'delete',
+		configuration
+	})
+	if (errors) {
+		return err(errors)
+	}
+	if (!permission.allowed) {
+		return err({ reason: 'Unauthorised' })
+	}
+	const schema = PermissionRequestSchema(data)
+	if (schema instanceof type.errors) {
+		return err({ reason: 'Invalid Data', message: schema.summary, id: 'invalid-data' })
+	}
+	const [errs, permissions] = await authorisation_module.deletePermission(schema)
+	if (errs !== null) {
+		return err(errs)
+	}
+	return ok(permissions)
+}
