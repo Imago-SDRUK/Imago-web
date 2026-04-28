a65af28701131444c2854104ba6d3453cb6b174e
artgp <git@arturoguzmanperez.com>
2026-04-28 17:27:45 +0100

    feat: add permissions create use case

--- apps/data_catalogue/src/lib/server/application/use_cases/permissions/create.ts
+++ apps/data_catalogue/src/lib/server/application/use_cases/permissions/create.ts
@@ -3,10 +3,10 @@ import {
 	PermissionRequestSchema,
 	type PermissionRequest
 } from '$lib/server/entities/models/permissions'
-import { type } from 'arktype'
+import { ArkErrors, type } from 'arktype'
 import type { AppContext } from '$lib/server/application/context'
 
-export const permissionCreateGetUseCase = async ({
+export const permissionCreateUseCase = async ({
 	data,
 	session,
 	configuration,
@@ -32,6 +32,60 @@ export const permissionCreateGetUseCase = async ({
 		return err({ reason: 'Invalid Data', message: schema.summary, id: 'invalid-data' })
 	}
 	const [errs, permissions] = await authorisation_module.createPermission(schema)
+	if (errs !== null) {
+		return err(errs)
+	}
+	return ok(permissions)
+}
+
+export const permissionsCreateUseCase = async ({
+	data,
+	session,
+	configuration,
+	authorisation_module
+}: {
+	data: PermissionRequest[]
+} & AppContext) => {
+	const [errors, permission] = await authorisation_module.authorise({
+		actor: session.identity.id,
+		namespace: 'Action',
+		object: 'permissions',
+		permits: 'create',
+		configuration
+	})
+	if (errors) {
+		return err(errors)
+	}
+	if (!permission.allowed) {
+		return err({ reason: 'Unauthorised' })
+	}
+	if (!Array.isArray(data)) {
+		return err({
+			reason: 'Invalid Data',
+			message: 'You need to provide an array',
+			id: 'permissions'
+		})
+	}
+	const validated = data
+		.map((record) => PermissionRequestSchema(record))
+		.reduce(
+			(acc, el) => {
+				if (el instanceof type.errors) {
+					acc.errors.push(el)
+					return acc
+				}
+				acc.data.push(el)
+				return acc
+			},
+			{ errors: [], data: [] } as { errors: ArkErrors[]; data: PermissionRequest[] }
+		)
+	if (validated.errors.length > 0) {
+		return err({ reason: 'Invalid Data', message: validated.errors[0].summary, id: 'arktype' })
+	}
+
+	const [errs, permissions] = await authorisation_module.createPermissions({
+		permissions: validated.data
+	})
 	if (errs !== null) {
 		return err(errs)
 	}
