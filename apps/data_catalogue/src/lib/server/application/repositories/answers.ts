23673c3465e5d86f70bbf3ada31ef6d8cac7823b
artgp <git@arturoguzmanperez.com>
2026-04-29 21:27:40 +0100

    fix: accept tx

--- apps/data_catalogue/src/lib/server/application/repositories/answers.ts
+++ apps/data_catalogue/src/lib/server/application/repositories/answers.ts
@@ -1,9 +1,15 @@
+import type { Transaction } from '$lib/db'
 import type { ErrTypes } from '$lib/server/entities/errors'
 import type { Answer, AnswerRequest } from '$lib/server/entities/models/questions'
 
 export type AnswersRepository = {
 	getAnswer: ({ id }: { id: string }) => Promise<[ErrTypes, null] | [null, Answer]>
-	createAnswer: ({ data }: { data: AnswerRequest }) => Promise<[ErrTypes, null] | [null, Answer]>
+	createAnswer: ({
+		data
+	}: {
+		data: AnswerRequest
+		tx?: Transaction
+	}) => Promise<[ErrTypes, null] | [null, Answer]>
 	createAnswers: ({
 		data
 	}: {
