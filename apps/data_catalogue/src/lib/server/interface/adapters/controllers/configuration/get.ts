32b05750afdba8c9d0398739c1a2e7db24c3efed
artgp <git@arturoguzmanperez.com>
2026-05-03 14:50:55 +0100

    chore: add logs

--- apps/data_catalogue/src/lib/server/interface/adapters/controllers/configuration/get.ts
+++ apps/data_catalogue/src/lib/server/interface/adapters/controllers/configuration/get.ts
@@ -1,12 +1,15 @@
 import { configurationGetUseCase } from '$lib/server/application/use_cases/configuration/get'
 import { err, ok } from '$lib/server/entities/errors'
 import { getConfigurationModule } from '$lib/server/modules/configuration'
+import { log } from '$lib/utils/server/logger'
 
 export const configurationGetController = async () => {
+	log.trace({ controller: 'configurationGetController' })
 	const [errors, res] = await configurationGetUseCase({
 		configuration_repository: getConfigurationModule()
 	})
 	if (errors !== null) {
+		log.error({ controller: 'configurationGetController', error: errors })
 		return err(errors)
 	}
 	return ok(res)
