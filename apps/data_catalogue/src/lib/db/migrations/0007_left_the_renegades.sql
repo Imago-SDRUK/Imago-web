bd868f769c439dd960cd9ef5ae2ff3d836d4e347
artgp <git@arturoguzmanperez.com>
2026-05-03 13:04:47 +0100

    fix: cast type

--- apps/data_catalogue/src/lib/db/migrations/0007_left_the_renegades.sql
+++ apps/data_catalogue/src/lib/db/migrations/0007_left_the_renegades.sql
@@ -6,7 +6,7 @@ ALTER TABLE "users_groups" DROP CONSTRAINT "users_to_groups_group_id_groups_id_f
 ALTER TABLE "users_groups" DROP CONSTRAINT "users_to_groups_user_id_group_id_pk";--> statement-breakpoint
 ALTER TABLE "groups" ALTER COLUMN "id" DROP DEFAULT;--> statement-breakpoint
 ALTER TABLE "groups" ALTER COLUMN "title" SET NOT NULL;--> statement-breakpoint
-ALTER TABLE "groups" ALTER COLUMN "datasets" SET DATA TYPE uuid[];--> statement-breakpoint
+ALTER TABLE "groups" ALTER COLUMN "datasets" SET DATA TYPE uuid[] USING groups.datasets::uuid[];--> statement-breakpoint
 ALTER TABLE "users_groups" ADD CONSTRAINT "users_groups_user_id_group_id_pk" PRIMARY KEY("user_id","group_id");--> statement-breakpoint
 ALTER TABLE "groups" ADD COLUMN "slug" text NOT NULL;--> statement-breakpoint
 ALTER TABLE "users_groups" ADD CONSTRAINT "users_groups_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
