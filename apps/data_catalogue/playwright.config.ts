c841159215551dde049ba408eaa0bfd07aee36c6
artgp <git@arturoguzmanperez.com>
2026-07-16 07:48:12 +0100

    chore: pw config
    chore: pw config

--- apps/data_catalogue/playwright.config.ts
+++ apps/data_catalogue/playwright.config.ts
@@ -1,9 +1,24 @@
-import { defineConfig } from '@playwright/test';
+import { defineConfig, devices } from '@playwright/test'
 
 export default defineConfig({
+	use: {
+		video: {
+			mode: 'on',
+			size: {
+				width: 1920,
+				height: 1080
+			}
+		}
+	},
+	projects: [
+		{
+			name: 'firefox',
+			use: { ...devices['Desktop Firefox'] }
+		}
+	],
 	webServer: {
 		command: 'npm run build && npm run preview',
 		port: 4173
 	},
 	testDir: 'e2e'
-});
+})
