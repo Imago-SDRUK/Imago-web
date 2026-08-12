6fb79c4aa3c02f629f371a0c2619d38d5fcfb1de
artgp <git@arturoguzmanperez.com>
2026-08-12 11:55:50 +0100

    feat: add products, products request and products resources
    fix: add missing migration

--- apps/data_catalogue/src/lib/server/entities/models/products.ts
+++ apps/data_catalogue/src/lib/server/entities/models/products.ts
@@ -1,7 +1,32 @@
-import { pgTable, uuid, timestamp, text, integer, index, primaryKey } from 'drizzle-orm/pg-core'
+import {
+	pgTable,
+	uuid,
+	timestamp,
+	text,
+	integer,
+	index,
+	primaryKey,
+	pgEnum,
+	jsonb
+} from 'drizzle-orm/pg-core'
 import { users } from './users'
 import { DateTime } from 'luxon'
 import { v7 as uuidv7 } from 'uuid'
+import { storages } from './storage'
+import { type } from 'arktype'
+
+export const product_resources_status_enum = pgEnum('product_resources_status_enum', [
+	'completed',
+	'error',
+	'requested',
+	'processing'
+])
+
+export const product_requests_status_enum = pgEnum('product_requests_status_enum', [
+	'notified',
+	'error',
+	'requested'
+])
 
 export const products = pgTable('products', {
 	created_by: uuid()
@@ -75,39 +100,167 @@ export const products_product_options = pgTable(
 	]
 )
 
-export const product_options = pgTable('product_options', {
-	created_by: uuid()
-		.references(() => users.id, { onDelete: 'no action' })
-		.notNull(),
-	updated_by: uuid()
-		.references(() => users.id, { onDelete: 'no action' })
-		.notNull(),
-	created_at: timestamp({
-		mode: 'date',
-		precision: 3,
-		withTimezone: true
-	})
-		.defaultNow()
-		.notNull(),
-	updated_at: timestamp({
-		mode: 'date',
-		precision: 3,
-		withTimezone: true
-	})
-		.defaultNow()
-		.notNull()
-		.$onUpdateFn(() => DateTime.now().toBSON()),
-	id: uuid()
-		.primaryKey()
-		.$defaultFn(() => uuidv7()),
-	name: text().unique().notNull(),
-	value: text().notNull(),
-	type: text().notNull()
+export const product_options = pgTable(
+	'product_options',
+	{
+		created_by: uuid()
+			.references(() => users.id, { onDelete: 'no action' })
+			.notNull(),
+		updated_by: uuid()
+			.references(() => users.id, { onDelete: 'no action' })
+			.notNull(),
+		created_at: timestamp({
+			mode: 'date',
+			precision: 3,
+			withTimezone: true
+		})
+			.defaultNow()
+			.notNull(),
+		updated_at: timestamp({
+			mode: 'date',
+			precision: 3,
+			withTimezone: true
+		})
+			.defaultNow()
+			.notNull()
+			.$onUpdateFn(() => DateTime.now().toBSON()),
+		id: uuid()
+			.primaryKey()
+			.$defaultFn(() => uuidv7()),
+		name: text().unique().notNull(),
+		value: text().notNull(),
+		// not enforcing any types
+		type: text().notNull()
+	},
+	(table) => [index('product_options_type_idx').on(table.type)]
+)
+
+export const product_requests = pgTable(
+	'product_requests',
+	{
+		created_by: uuid()
+			.references(() => users.id, { onDelete: 'cascade' })
+			.notNull(),
+		updated_by: uuid()
+			.references(() => users.id, { onDelete: 'cascade' })
+			.notNull(),
+		created_at: timestamp({
+			mode: 'date',
+			precision: 3,
+			withTimezone: true
+		})
+			.defaultNow()
+			.notNull(),
+		updated_at: timestamp({
+			mode: 'date',
+			precision: 3,
+			withTimezone: true
+		})
+			.defaultNow()
+			.notNull()
+			.$onUpdateFn(() => DateTime.now().toBSON()),
+		status: product_requests_status_enum().default('requested').notNull(),
+		id: uuid()
+			.primaryKey()
+			.$defaultFn(() => uuidv7()),
+		product_id: uuid()
+			.references(() => products.id)
+			.notNull(),
+		version: text().notNull(),
+		year: integer().notNull(),
+		options: uuid()
+			.array()
+			// .references(() => product_options.id, { onDelete: 'no action' })
+			.default([])
+			.notNull()
+	},
+	(table) => [index('product_requests_product_id_idx').on(table.product_id)]
+)
+
+export const product_resources = pgTable(
+	'product_resources',
+	{
+		created_by: uuid()
+			.references(() => users.id, { onDelete: 'cascade' })
+			.notNull(),
+		updated_by: uuid()
+			.references(() => users.id, { onDelete: 'cascade' })
+			.notNull(),
+		created_at: timestamp({
+			mode: 'date',
+			precision: 3,
+			withTimezone: true
+		})
+			.defaultNow()
+			.notNull(),
+		updated_at: timestamp({
+			mode: 'date',
+			precision: 3,
+			withTimezone: true
+		})
+			.defaultNow()
+			.notNull()
+			.$onUpdateFn(() => DateTime.now().toBSON()),
+		status: product_resources_status_enum().default('requested').notNull(),
+		id: uuid()
+			.primaryKey()
+			.$defaultFn(() => uuidv7()),
+		product_id: uuid()
+			.references(() => products.id)
+			.notNull(),
+		version: text().notNull(),
+		year: integer().notNull(),
+		options: uuid()
+			.array()
+			// .references(() => product_options.id, { onDelete: 'no action' })
+			.default([])
+			.notNull(),
+		path: text(),
+		filename: text(),
+		metadata: jsonb(),
+		messages: jsonb(),
+		storage: uuid().references(() => storages.id, { onDelete: 'set null' })
+	},
+	(table) => [
+		index('product_resources_product_id_idx').on(table.product_id),
+		index('product_resources_storage_idx').on(table.storage)
+	]
+)
+
+/**
+ * {
+ * status: 'error' | 'completed' | 'processing'
+ * filename: string,
+ * path: string,
+ * metadata: ?,
+ * messages: {type: 'error' | 'process', message: string}
+ * }
+ **/
+
+export const ProductResourceUpdateMessageSchema = type({
+	status: "'trace' | 'debug' | 'info' | 'warn'| 'error' | 'fatal'",
+	message: 'string'
 })
 
+export const ProductResourceUpdateCompleteSchema = type({
+	status: "'error' | 'completed' | 'processing'",
+	filename: 'string',
+	path: 'string',
+	'metadata?': 'object'
+})
+
+export const ProductResourceUpdateSchema = type.or(
+	ProductResourceUpdateMessageSchema,
+	ProductResourceUpdateCompleteSchema
+)
+
 export type Product = typeof products.$inferSelect
-export type ProductRequest = typeof products.$inferInsert
+export type ProductInsert = typeof products.$inferInsert
 export type ProductOption = typeof product_options.$inferSelect
-export type ProductOptionRequest = typeof product_options.$inferInsert
+export type ProductOptionInsert = typeof product_options.$inferInsert
 export type ProductsProductOptions = typeof products_product_options.$inferSelect
-export type ProductsProductOptionsRequest = typeof products_product_options.$inferInsert
+export type ProductsProductOptionsInsert = typeof products_product_options.$inferInsert
+export type ProductRequest = typeof product_requests.$inferSelect
+export type ProductRequestInsert = typeof product_requests.$inferInsert
+export type ProductResource = typeof product_resources.$inferSelect
+export type ProductResourceInsert = typeof product_resources.$inferInsert
